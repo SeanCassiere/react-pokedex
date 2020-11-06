afd5c99fa0feb6d7e8375588f86eb65f508aa0ac
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2020-11-07 03:18:43 +0530

    Image Error Handling and Spinner

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 
 import styled from 'styled-components';
 
+import spinner from '../pokemon/loading.gif';
+
 const Sprite = styled.img`
   width: 100%;
+  display: none;
 `;
 
 export default class PokemonCard extends Component {
   state = {
     name: '',
     imageUrl: '',
+    imageLoading: true,
+    tooManyRequests: false,
     pokemonIndex: ''
   }
 
@@ -30,11 +35,35 @@ export default class PokemonCard extends Component {
       <div className="col-md-3 col-sm6 mb-5">
         <div className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          { this.state.imageLoading ? (
+            <img
+              src={spinner}
+              style={{
+                width: '2rem',
+                height: '2rem'
+              }}
+              className="card-img-top rounded mx-auto d-block mt-2"
+            />
+          ) : null }
           <Sprite
             className="card-img-top rounded mx-auto mt-2"
+            onLoad={() => this.setState({ imageLoading: false })}
+            onError={() => this.setState({ tooManyRequests: true })}
             src={this.state.imageUrl}
-            alt={this.state.name}>
+            alt={this.state.name}
+            style={
+              this.state.tooManyRequests ? { display: 'none'} :
+              this.state.imageLoading ? null : {
+                display: 'block'
+              }
+            }
+            >
           </Sprite>
+          { this.state.tooManyRequests ? (
+            <h6 className="mx-auto">
+              <span className="badge badge-danger mt-2">Too Many Requests</span>
+            </h6>
+          ) : null }
           <div className="card-body mx-auto">
             <h5 className="card-title">
               {this.state.name

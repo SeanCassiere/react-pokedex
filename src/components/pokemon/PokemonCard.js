d3ecf6ba27680b1c87c93bc181ef0ec5bee21521
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2020-11-07 02:57:19 +0530

    Finding with Images

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+import styled from 'styled-components';
+
+const Sprite = styled.img`
+  width: 100%;
+`;
+
 export default class PokemonCard extends Component {
   state = {
     name: '',
@@ -7,14 +13,38 @@ export default class PokemonCard extends Component {
     pokemonIndex: ''
   }
 
-  render() {
+  componentDidMount() {
     const { name, url } = this.props;
+    const pokemonIndex = url.split('/')[url.split('/').length - 2];
+    const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+  
+    this.setState({
+      name,
+      imageUrl,
+      pokemonIndex
+    })
+  }
 
+  render() {
     return (
-      <div className="col-md-3 col-sm6-mb-5">
+      <div className="col-md-3 col-sm6 mb-5">
         <div className="card">
-          <div className="card-header">
-            <h1>{name}</h1>
+          <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <Sprite
+            className="card-img-top rounded mx-auto mt-2"
+            src={this.state.imageUrl}
+            alt={this.state.name}>
+          </Sprite>
+          <div className="card-body mx-auto">
+            <h5 className="card-title">
+              {this.state.name
+                .toLowerCase()
+                .split(' ')
+                .map(
+                  letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                ).join(' ')
+              }
+            </h5>
           </div>
         </div>
       </div>

070368524f1509c0b0c7ce7d4453f08b7d037450
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2020-11-08 20:19:58 +0530

    Added GitHub URL to Navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {IconContext} from "react-icons"
+import { IconContext } from "react-icons"
 import { CgPokemon } from 'react-icons/cg';
+import { FaGithubSquare } from 'react-icons/fa';
 // import styled from 'styled-components';
 
 export default class NavBar extends Component {
@@ -14,6 +15,21 @@ export default class NavBar extends Component {
             <IconContext.Provider value={{ style: { fontSize: '35px', color: "#003A70" } }}>
               <a className="navbar-brand" href="/"><CgPokemon /> PokeDex</a>
             </IconContext.Provider>
+            <div className="navbar-nav">
+              <IconContext.Provider value={{ style: { fontSize: '18px' } }}>
+                <a
+                  className="nav-item nav-link"
+                  href="https://github.com/SeanCassiere/react-pokedex"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    marginTop: '0.2rem'
+                  }}
+                >
+                  <FaGithubSquare style={{marginBottom: '0.23rem'}} />&nbsp;Github/SeanCassiere
+                </a>
+              </IconContext.Provider>
+            </div>
           </div>
         </nav>
       </div>

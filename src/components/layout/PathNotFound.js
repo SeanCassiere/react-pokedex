4bb845ad5f03f2075d249ed9c7aaa306043dea45
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2020-11-09 05:05:04 +0530

    404 Styling

--- src/components/layout/PathNotFound.js
+++ src/components/layout/PathNotFound.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class PathNotFound extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-12 text-muted">
-          <h1 className="display-4 font-weight-bold">Error: Path Error</h1>
-          <h5>Path not found</h5>
+      <div className="row" style={{paddingTop: '35vh'}}>
+        <div className="col-12 text-muted text-center">
+          <p className="font-weight-bold" style={{fontSize: '2rem'}}>
+            <span className="badge badge-danger" style={{fontSize: '2rem'}}>Error</span>
+          </p>
+          <p className="font-weight-bold mt-4">This path does not exist or is not directly accessible.</p>
+          <p className="font-weight-bold">Please return back to the <Link to="/">homepage</Link>.</p>
         </div>
       </div>
     )

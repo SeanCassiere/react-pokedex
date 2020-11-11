fbae1ada3c0f1cd7a5e1216bf98ff5964d2f9e24
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-11 21:31:59 +0530

    React-Helmet for Loading

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Helmet from 'react-helmet';
 
 export default function Loading(props) {
   return (
     <div className="row" style={{paddingTop: '35vh'}}>
+      <Helmet>
+        <title>Pokedex: Loading</title>
+      </Helmet>
       <div className="col-12 text-center">
         <p className="badge badge-warning p-3" style={{fontSize: '1.6rem'}}>{props.textItem}</p>
       </div>
